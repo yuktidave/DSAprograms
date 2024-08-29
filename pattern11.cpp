3e00b7a17150246dba452201c5c7fe4aefb5f044
Yukti Dave <yuktidave1212@gmail.com>
2024-08-29 18:44:39 +0530

    Add files via upload

---
+++ pattern11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 1;
+    cin >> n;
+
+    int row = 1;
+    while(row<=n){
+
+        int col = 1;
+        while(col<=n){
+
+            char ch = 'A' + row - 1;
+            cout << ch;
+            col++;
+
+        }
+        cout << endl;
+        row++;
+    }
+
+
+}
